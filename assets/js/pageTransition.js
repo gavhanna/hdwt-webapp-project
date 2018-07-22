6f0ac47cfb9473ca4119b9ad27d90cd1caeab65c
Gav Hanna <gavhanna@gmail.com>
2018-07-22 15:06:54 +0100

    added page transitions

---
+++ assets/js/pageTransition.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  pageTransition();
+});
+
+const pageTransition = function () {
+  const pageBody = document.querySelector("body");
+  const anchors = document.querySelectorAll("a");
+
+  pageBody.classList.add("enter-transition");
+
+  anchors.forEach(a => {
+    a.addEventListener("click", handleClick);
+  });
+
+  function handleClick(e) {
+    //e.preventDefault();
+    pageBody.classList.remove("enter-transition")
+    pageBody.classList.add("exit-transition");
+    console.log(e.target.href);
+    if (e.target.href) {
+      setTimeout(() => {
+        window.location.href = e.target.href;
+      }, 300);
+    }
+  }
+};
+
